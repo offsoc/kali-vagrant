b38a4f978ab4e59232c350ddf3dcc3a297c8be03
J-C Lariviere <jclariviere2@gmail.com>
2020-02-27 12:24:37 -0500

    Remove guest-additions install script (it gets auto-installed now)

--- scripts/virtualbox.sh
+++
@@ -1,5 +0,0 @@
-# Install VirtualBox Guest Additions
-apt-get update
-# kernel updates frequently and guest additions may update to latest headers, so make sure the kernel version matches the headers version
-apt-get install -y linux-headers-amd64 linux-image-amd64
-apt-get install -y virtualbox-guest-x11
